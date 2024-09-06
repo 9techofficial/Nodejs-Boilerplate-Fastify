1b257dd2004637fd111d5fbda93c7c09ea8ca5a8
ankush0a <agrawalankush65@gmail.com>
2024-09-06 15:00:52 +0530

    Add multiple environment file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
+import dotenv from 'dotenv';
 import fastify from 'fastify';
 import { connectDB } from './utils/db';
+
+// implement fastify
 const app = fastify({ logger: false });
+
+// Load the appropriate .env file based on NODE_ENV
+const nodeEnv = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `.env.${nodeEnv}` });
 
 const start = async () => {
   try {
     await connectDB();
-    await app.listen({ port: 3000 });
-    console.log('Server listening on http://localhost:3000');
+    await app.listen({ port: Number(process.env.PORT) || 3000 });
+    console.log(`Server running in ${process.env.NODE_ENV} mode at http://localhost:${process.env.PORT}`);
   } catch (error) {
     app.log.error(error);
     process.exit(1);
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/boilerplate');
+    await mongoose.connect(process.env.DB_URI!);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB: ', error);
